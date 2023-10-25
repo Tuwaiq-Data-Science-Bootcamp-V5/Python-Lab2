5d5ac208cd50f8f8f4646dc9ec6aef803098f3fc
Faisal Alotaibi <faisalota@gmail.com>
2023-10-25 13:49:47 +0300

    lab2

---
+++ lab2.py
@@ -0,0 +1,14 @@
+movie = "Interstellar"
+
+rating = 3
+
+popScore = 72.65
+
+if rating >=4 and popScore>80:
+        print("Highly recommended")
+elif rating>=3 and popScore>70:
+        print("i recommend it, it is good")
+elif rating <=2 and popScore>60:
+        print("You should check it out!")
+elif rating <=2 and popScore<50:
+        print("don't watch it")
